fb80d414a18dea85b3aa552d34ab755866741474
Mac VMware Centos7 <512113110@qq.com>
2022-03-13 14:34:35 +0800

    cmd auto submit

--- src/MiddlewareTool.php
+++ src/MiddlewareTool.php
@@ -15,13 +15,18 @@ class MiddlewareTool {
      * @param [type] $request
      * @return void
      */
-    static public function getToken($request,$domain="sidoc.cn") {
+    static public function getToken($request) {
 
-        // 0.1> 获取token（分别兼容从请求头、Cookie和请求参数中来获取token）
+        // 0.1> 从请求头中获取token（分别兼容从请求头、Cookie和请求参数中来获取token）
         $token = $request->header("authorization");
+        if(!empty($token)){
+            self::configCookie($token); // 配置Cookie以兼容未能前后分离的项目
+            return $token;
+        }
 
         // 0.2> 若从请求头无法获取token，则尝试从Cookie中获取token，以兼容未能前后分离的项目；
         if(empty($token)){ 
+            // 前后端未分离的项目必须使用Cookie,否则页面跳转将没有权限，请求头中token仅用于为接口授权
             $token = Cookie::get("authorization");
         }
 
@@ -33,18 +38,7 @@ class MiddlewareTool {
         // 为了尽量保护token安全，token名称尽量使用不易被发现和使用的名称，此处使用t1作为token名称
         if(empty($token)){ 
             $token   = $request->param("t1"); 
-            $expires = $request->param("e1"); 
-            
-            // 在cookie中保存token
-            // cookie域名有效性设置：http://www.cnblogs.com/fsjohnhuang/archive/2011/11/22/2258999.html
-            // setcookie("cookieName","cookieValue, 0, "/", ".test.com"); // 过期时间为0，表示浏览器关闭时销毁cookie;
-            
-            // 作用域："abc.com" 可以在abc.com主域名之下的多级子域名有效
-            //       ".abc.com" 只能在二级域名以及"www.abc.com"下有效
-            // 参数secure：cookie是否只能通过https发送
-            // 参数httponly：是否只能通过http协议访问cookie
-            // 配置详见：https://www.php.net/manual/zh/function.setcookie.php
-            setcookie("authorization",$token,$expires,'/',$domain,false,true);
+            self::configCookie($token);
         }
         return $token;
     }
@@ -56,13 +50,40 @@ class MiddlewareTool {
      * @return void
      */
     static public function verifyToken($token) {
-
         $authInfo = null;
         if($token){
             // 被远程请求的服务必须删除Trace，否则将无法输出纯字符串，以致加解密错误，详见：https://www.sidoc.cn/doc/1124.html
             $authInfo = RemoteCallTools::request(Env::get('SIDOC_USER_SERVICE')."/auth",['token'=>$token]);
         }
         return $authInfo;
+    }
+
+
+    /**
+     * 配置Cookie
+     *
+     * @param [type] $token
+     * @param [type] $domain
+     * @param [type] $expires
+     * @return void
+     */
+    static private function configCookie($token) {
+
+        // 默认设为1周有效期
+        // 微调token过期时间，使其在凌晨4点左右失效；避免在白天用户使用时突然失效，影响用户体验
+        $hour = date('G',time()); // 获取当天时间(小时,24小时制)
+        $expires = time() + 604800 + ((24 - $hour + 4) * 3600); // 有效时长(秒)，此处为一周(604800秒)，一周后凌晨4点过期
+        
+        // 在cookie中保存token
+        // cookie域名有效性设置：http://www.cnblogs.com/fsjohnhuang/archive/2011/11/22/2258999.html
+        // setcookie("cookieName","cookieValue, 0, "/", ".test.com"); // 过期时间为0，表示浏览器关闭时销毁cookie;
+
+        // 作用域："abc.com" 可以在abc.com主域名之下的多级子域名有效
+        //       ".abc.com" 只能在二级域名以及"www.abc.com"下有效
+        // 参数secure：cookie是否只能通过https发送
+        // 参数httponly：是否只能通过http协议访问cookie
+        // 配置详见：https://www.php.net/manual/zh/function.setcookie.php
+        setcookie("authorization",$token,$expires,'/',$domain,false,true);
     }
 
 

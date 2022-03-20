f11d79840ab6416fdfb4cc5d51bb1e6e2fb0dccc
Mac VMware Centos7 <512113110@qq.com>
2022-03-20 13:27:39 +0800

    cmd auto submit

--- src/DatabaseBackup.php
+++ src/DatabaseBackup.php
@@ -12,13 +12,16 @@ class DatabaseBackup {
 
         $t1 = microtime(true);
 
+        // 0.0> 配置数据库登录信息
+        // 高版本的MySql在备份数据时，必须从配置文件中读取敏感信息，此举是为了提高数据库安全性
+        // 此处将数据库登录信息写入至配置文件中，以供数据库备份命令调用
+        self::configLoginInfo();
+
         // 0.1> 备份数据库
         $databaseName = Env::get('DATABASE.DATABASE');
-        $username = Env::get('DATABASE.USERNAME');
-        $password = Env::get('DATABASE.PASSWORD');
         $time = date('Y-m-d_H:i:s');
         $backupPath  = app_path()."dataBackup/{$databaseName}-{$time}.sql";
-        $result = shell_exec("mysqldump -u{$username} -p{$password} {$databaseName} > {$backupPath} 2>&1"); // '2>&1'是让执行管道输出结果。
+        $result = shell_exec("mysqldump --defaults-extra-file=/var/www/html/sidoc/app/dataBackup/mysql.conf {$databaseName} > {$backupPath} 2>&1"); // '2>&1'是让执行管道输出结果。
         echo $result;
 
         // 0.2> 加密sql文件
@@ -34,10 +37,26 @@ class DatabaseBackup {
         self::updateTask($taskId,$timeConsuming);
     }
 
+    // 配置数据库登录信息
+    static private function configLoginInfo(){
+
+        $configFile  = app_path()."dataBackup/mysql.conf";
+        $hostname = Env::get('DATABASE.HOSTNAME');
+        $username = Env::get('DATABASE.USERNAME');
+        $password = Env::get('DATABASE.PASSWORD');
+        
+        // 以写入方式打开文件，并清除文件内容，如果文件不存在则尝试创建之
+        $nginxServerConfig = fopen($configFile, "w");
+        fwrite($nginxServerConfig,"[client]               \n");
+        fwrite($nginxServerConfig,"host='{$hostname}'     \n");
+        fwrite($nginxServerConfig,"user='{$username}'     \n");
+        fwrite($nginxServerConfig,"password='{$password}' \n");
+        fclose($nginxServerConfig);
+    }
+
     // 备份至对象存储
     static private function backupToCos($projectName) {
 
-        // 0.2> 保存备份数据至对象存储 -------------------------------------------------------------
         $folder = app_path()."dataBackup";
         $handle = opendir($folder);
         while($file=readdir($handle)){
@@ -48,7 +67,7 @@ class DatabaseBackup {
                 TencenyunCos::pushObject($projectName."/database-backup/{$file}",$path);
             }
             if($suffix == 'sql' || $suffix == 'enc'){
-                unlink($path); // 删除删除
+                // unlink($path); // 删除删除
             }
         }
          
@@ -57,7 +76,6 @@ class DatabaseBackup {
     // 更新任务
     static private function updateTask($taskId,$timeConsuming) {
    
-        // 0.3> 更新任务状态 --------------------------------------------------------------------
         $arr['id'] = $taskId;
         $arr['des'] = "数据备份完成，耗时".$timeConsuming."秒";
         $arr['status'] = "normal";
--- tests/FileEncryptionTest.php
+++ tests/FileEncryptionTest.php
@@ -9,4 +9,5 @@ require_once "../vendor/autoload.php";
 use sidoc\FileEncryption;
 
 // FileEncryption::encryptFile("FileEncryptionTest_data1.sql","111","FileEncryptionTest_data1");
-FileEncryption::decryptFile("FileEncryptionTest_data1","111","FileEncryptionTest_data1_1");
+// FileEncryption::decryptFile("FileEncryptionTest_data1","111","FileEncryptionTest_data1_1");
+FileEncryption::decryptFile("FileEncryptionTest_data1.sql.enc","dc7ff283-2722-4248-8cff-da2b91ffcee4","FileEncryptionTest_data1.sql.enc");

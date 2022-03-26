d85bd1f481e694b1d9fcebe7904a0878e5235f72
Mac VMware Centos7 <512113110@qq.com>
2022-03-26 23:25:13 +0800

    cmd auto submit

--- src/SSLService.php
+++ src/SSLService.php
@@ -20,7 +20,7 @@ use Throwable;
  * SSL证书检查和续期
  */
 class SSLService{
-    
+
     /**
      * 检查域名的SSL证书剩余有效时间，并自动续期
      *
@@ -29,85 +29,71 @@ class SSLService{
      * @param string $verifyDomain 用于验证域名的SSL证书是否过期的域名，部分域名本身可能无法验证其SSL是否过期，如：user.sidoc.cn，验证其证书是否过期必须使用service.user.sidoc.cn,因为user.sidoc.cn域名本身可能暂未使用或未被解析
      * @return void
      */
-    static public function check($domain,$cdnDomainList=[],$verifyDomain=null){
+    static public function check($domain,$cdnDomainList=[]){
 
-        // 0.1> 获取域名指向的IP
-        $ips = self::getIps($domain);
-        if (count($ips) === 0) {
-            // 命令行程序抛出的异常不会被全局捕获，因此主动上报
-            WorkWechat::reportException(new Exception('无法获取域名$domain指向的IP'));
-            $des = "无法获取域名".$domain."指向的IP地址";
-            self::updateTask($domain,$des,date("Y-m-d H:i:s"),"error");
+        // 0.1> 获取SSL证书过期时间 -----------------------------
+        $diffDay = self::getExpiredTime($domain);
+        if($diffDay==false){
             return;
         }
 
-        // 0.2> 获取证书到期时间，并续期
-        foreach ($ips as $ip) {
-            if($verifyDomain){
-                $cert = self::getCert($ip, $domain, $verifyDomain); // 获取证书信息
-            }else{
-                $cert = self::getCert($ip, $domain, $domain); // 获取证书信息
+        // 0.2> 当SSL证书有效期小于20天时，则申请或更新证书 -----------------------------------
+        if($diffDay <=20){ 
+            // 申请SSL证书
+            if(self::applySSL($domain) !== true){
+                return;
             }
-            if (!$cert) {
-                $errors[] =  $domain . '@' . $ip . " :: FAILED TO GET CERTIFICATE INFORMATION\n";
-                self::updateTask($domain,"申请SSL证书时发生错误，无法获取到域名{$domain}的SSL证书信息，请查看域名或验证域名是否可访问",date("Y-m-d H:i:s"),"error");
-                continue;
+            // 部署CDN证书
+            foreach($cdnDomainList as $cdnDomain){
+                self::deployToCDN($domain,$cdnDomain);
             }
-
-            // 获取SSL证书到期时间
-            $validTo = new \DateTime("@" . $cert['validTo_time_t']);
-            $now = new \DateTime('now', new \DateTimeZone('UTC'));
-            $diff = $now->diff($validTo);
-            if($diff->days <=20){ // 当SSL证书有效期小于20天时，则更新证书
-                $r = self::applySSL($domain);
-                if($r !== true){
-                    continue;
-                }
-                
-                // 部署CDN证书
-                if(count($cdnDomainList) > 0){
-                    foreach($cdnDomainList as $cdnDomain){
-                        self::deployToCDN($domain,$cdnDomain);
-                    }
-                }
-                self::updateTask($domain,"证书申请完成，下一次证书检查后，可查看其剩余过期时间",date("Y-m-d H:i:s"),"normal");
-            }else{
-                $des = "证书还有".$diff->days."天过期，暂时无须更新";
-                self::updateTask($domain,$des,date("Y-m-d H:i:s"),"normal");
-            }
+            self::updateTask($domain,"证书申请完成，过期时间为".intval(self::getExpiredTime($domain))."天后",date("Y-m-d H:i:s"),"normal");
+        }else{
+            
+            $des = "证书还有".intval($diffDay)."天过期，暂时无须更新";
+            self::updateTask($domain,$des,date("Y-m-d H:i:s"),"normal");
         }
     }
 
-    // 获取域名指向的IP
-    static private function getIps($domain) {
-        $ips = [];
-        $dnsRecords = dns_get_record($domain, DNS_A + DNS_AAAA);
-        foreach ($dnsRecords as $record) {
-            if (isset($record['ip'])) {
-                $ips[] = $record['ip'];
-            }
-            if (isset($record['ipv6'])) {
-                $ips[] = '[' . $record['ipv6'] . ']'; // bindto of 'stream_context_create' uses this format of ipv6
-            }
-        }
-        return $ips;
-    }
+    /**
+     * 获取域名的过期时间
+     *
+     * @param [type] $domain
+     * @return void
+     */
+    static private function getExpiredTime($domain){
 
-    // 获取域名的SSL证书信息，若域名未安装SSL证书，则此方法会抛出异常
-    static private function getCert($ip, $domain, $verifyDomain=null) {
-        try{
-            $d = $verifyDomain?$verifyDomain:$domain;
-            $g = stream_context_create(["ssl" => ["capture_peer_cert" => true], 'socket' => ['bindto' => $ip]]);
-            $r = stream_socket_client("ssl://{$d}:443", $errno, $errstr, 30, STREAM_CLIENT_CONNECT, $g);
-            $cont = stream_context_get_params($r);
-            return openssl_x509_parse($cont["options"]["ssl"]["peer_certificate"]);
-        }catch(Throwable $e){
-            // 命令行程序抛出的异常不会被全局捕获，因此主动上报
-            WorkWechat::reportException($e);
-            $des = "无法获取域名".$domain."的SSL证书信息";
-            self::updateTask($domain,$des,date("Y-m-d H:i:s"),"error");
+        // 0.1> 获取SSL证书信息 -----------------------
+        $command = "openssl x509 -in /var/www/ssl/$domain -noout -dates";
+        exec($command,$out,$status); 
+        // status是执行的状态 0为成功 1为失败
+        if($status != 0){
+           Log::error($out);
+           $msg = "获取域名".$domain."的SSL证书过期时间时发生错误:".json_encode($out,JSON_UNESCAPED_UNICODE);  // json_encode默认会将中文转为unicode编码，JSON_UNESCAPED_UNICODE配置声明保留原有中文，不要转为unicode
+           // 命令行程序抛出的异常不会被全局捕获，因此主动上报
+           WorkWechat::reportException(new Exception($msg));
+           self::updateTask($domain,$msg,date("Y-m-d H:i:s"),"error");
+           return false;
         }
+
+        // 0.2> 取出SSL证书的过期时间 -----------------------
+        foreach($out as $item){
+            if(strpos($item,'notAfter') !== false){
+                $time = explode("=",$item)[1];
+                $time = strtotime($time);
+                $diff = $time - time();
+                $diff = $diff/86400; // 秒转天
+                return $diff;
+            }
+        }
+
+        // 0.3> 若无法获取SSL证书过期时间，则上报错误 ------------
+        $msg = "无法获取域名".$domain."的SSL证书过期时间";
+        // 命令行程序抛出的异常不会被全局捕获，因此主动上报
+        WorkWechat::reportException(new Exception($msg));
+        self::updateTask($domain,$msg,date("Y-m-d H:i:s"),"error");
     }
+    
 
     // 申请SSL证书，并完成本机安装
     static private function applySSL($domain){

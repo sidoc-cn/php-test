00a5b2fa943907478bf8abb72629538f5bd995ce
Mac VMware Centos7 <512113110@qq.com>
2022-03-28 11:25:53 +0800

    cmd auto submit

--- src/CaptchaTool.php
+++ src/CaptchaTool.php
@@ -8,13 +8,14 @@ use Gregwar\Captcha\CaptchaBuilder;
 class CaptchaTool {
 
     /**
-     * 生成并输出图形验证码，同时返回验证码文本
+     * 生成并输出图形验证码
+     * 此函数同时返回验证码校验key，客户端需要提交此key以便服务端从redis中读取验证码，并进行校验
      * 
      * 官网：https://github.com/Gregwar/Captcha
      *
      * @return void
      */
-    static public function image($businessType) {
+    static public function image() {
 
         $builder = new CaptchaBuilder();
         // 启用或禁用插值（默认启用），禁用会提交效率，但图像会更丑；
@@ -25,10 +26,37 @@ class CaptchaTool {
         // 输出验证码图片
         $builder->output();
 
-        $key = $businessType + Tools::get_real_ip();
-
-        return $e->getPhrase(); // 获取验证码
+        // 保存验证码文本,5分钟后自动销毁
+        $key = "captcha_".Guid::guidString(Tools::get_real_ip());
+        $redis = RedisManger::instance(['select'=>1]);
+        $redis->setex($key,300,$e->getPhrase());
+        return $key;
     }
+
+    /**
+     * 图片验证码校验
+     *
+     * @param [type] $key  用于读取验证码的key
+     * @param [type] $code 用户输入的验证码
+     * @return void
+     */
+    static public function imageVerify($key,$code) {
+        
+        $redis = RedisManger::instance(['select'=>1]);
+        $captchaCode = $redis->get($key);
+        if(empty($captchaCode)){
+            return false;
+        }
+
+        $captchaCode = strtolower($captchaCode); // 转小写
+        if($captchaCode === strtolower($code)){
+            return true;
+        }
+        return false;
+    }
+
+
+
 
     /**
      * 行为验证
